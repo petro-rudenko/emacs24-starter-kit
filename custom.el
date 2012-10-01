9d562c74c72693071c0842ab6e0859b149f97695
Peter <petro.rudenko@gmail.com>
2012-10-01 23:16:23 +0300

    My customization

---
+++ custom.el
@@ -0,0 +1,35 @@
+(setq stack-trace-on-error t)
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(ecb-auto-activate t)
+ '(ecb-layout-name "left3")
+ '(ecb-options-version "2.40")
+ '(ecb-process-non-semantic-files t)
+ '(ecb-source-file-regexps (quote ((".*" ("\\(^\\(\\.\\|#\\)\\|\\(~$\\|\\.\\(elc\\|obj\\|o\\|pyc\\|class\\|lib\\|dll\\|a\\|so\\|cache\\)$\\)\\)") ("^\\.\\(emacs\\|gnus\\)$")))))
+ '(ecb-sources-exclude-cvsignore nil)
+ '(ecb-tip-of-the-day nil)
+ '(ecb-windows-width 0.2)
+ '(erc-notice-highlight-type nil)
+ '(inhibit-startup-screen t)
+ '(scroll-bar-mode (quote right)))
+
+(fset 'yes-or-no-p 'y-or-n-p) 
+(setq file-name-coding-system 'utf-8)
+
+
+;;Line number column
+(autoload 'linum-mode "linum" "toggle line numbers on/off" t) 
+(global-linum-mode)
+
+(setq auto-mode-alist (cons '("\\.m$" . octave-mode) auto-mode-alist)) 
+(setq browse-url-generic-program (executable-find "google-chrome")
+browse-url-browser-function 'browse-url-generic)
+
+(require `ahg)
+(require 'color-theme-gruber-darker) 
+(color-theme-gruber-darker)
+(starter-kit-load "python")
+(desktop-save-mode 0)
---
+++ starter-kit-python.el
@@ -0,0 +1,88 @@
+;===============================================================================
+;basic settings
+
+
+(setq dotfiles-dir (file-name-directory
+                   (or load-file-name (buffer-file-name))))
+(setq pythonEmacsDir (concat dotfiles-dir "python/"))
+(add-to-list `load-path pythonEmacsDir)
+
+
+(load-file (concat pythonEmacsDir "emacs-for-python/epy-init.el"))
+(require 'epy-setup) ;; It will setup other loads, it is required!
+(require 'epy-python) ;; If you want the python facilities [optional]
+(require 'epy-completion) ;; If you want the autocompletion settings [optional]
+(require 'epy-editing) ;; For configurations related to editing [optional]
+(require 'epy-bindings) ;; For my suggested keybindings [optional]
+;(epy-setup-checker (concat pythonEmacsDir "/pychecker.sh"))
+
+
+(when (load "flymake" t)
+  (defun flymake-pylint-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list (concat pythonEmacsDir "flymake/pyflymake.py") (list local-file))))
+      ;;     check path
+
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pylint-init)))
+
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+
+(epy-django-snippets)
+(epy-setup-ipython)
+(add-to-list 'load-path (concat pythonEmacsDir "pony-mode/src/"))
+(require 'pony-mode)
+
+;;  (add-to-list `load-path (concat pythonEmacsDir  "/yasnippet-0.6.1c/"))
+;;  ;(add-to-list `ac-dictionary-directories "~/.emacs.d/dict")
+;;  (require `auto-complete-config)
+;;  (ac-config-default)
+;;  (require `yasnippet)
+;;  (yas/initialize)
+;;  (yas/load-directory (concat pythonEmacsDir "/yasnippet-0.6.1c/snippets"))
+;;  (require `smart-operator)
+;; ;; ;===============================================================================
+;;  ;(require `auto-complete-config)
+
+;;  (load-library "init_python")
+
+;; ;; ;;Pony mode
+(add-to-list `load-path (concat pythonEmacsDir "pony-mode/src"))
+(require 'pony-mode)
+
+
+(defun python--add-debug-highlight ()
+  "Adds a highlighter for use by `python--pdb-breakpoint-string'"
+  (highlight-lines-matching-regexp "## DEBUG ##\\s-*$" 'hi-red-b))
+ 
+(add-hook 'python-mode-hook 'python--add-debug-highlight)
+ 
+(defvar python--pdb-breakpoint-string "import ipdb; ipdb.set_trace() ## DEBUG ##"
+  "Python breakpoint string used by `python-insert-breakpoint'")
+ 
+(defun python-insert-breakpoint ()
+  "Inserts a python breakpoint using `pdb'"
+  (interactive)
+  (back-to-indentation)
+  ;; this preserves the correct indentation in case the line above
+  ;; point is a nested block
+  (split-line)
+  (insert python--pdb-breakpoint-string))
+(define-key python-mode-map (kbd "<f5>") 'python-insert-breakpoint)
+ 
+(defadvice compile (before ad-compile-smart activate)
+  "Advises `compile' so it sets the argument COMINT to t
+if breakpoints are present in `python-mode' files"
+  (when (derived-mode-p major-mode 'python-mode)
+    (save-excursion
+      (save-match-data
+        (goto-char (point-min))
+        (if (re-search-forward (concat "^\\s-*" python--pdb-breakpoint-string "$")
+                               (point-max) t)
+            ;; set COMINT argument to `t'.
+            (ad-set-arg 1 t))))))
+
