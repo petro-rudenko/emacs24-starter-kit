a613e4efb43b6ba48efb86ffe69ac7b61c0289f2
peter <peter@peter.(none)>
2012-12-20 23:47:59 +0200

    Scala mode2

--- starter-kit-scala.el
+++ starter-kit-scala.el
@@ -3,7 +3,7 @@
                    (or load-file-name (buffer-file-name))))
 (add-to-list 'load-path (concat dotfiles-dir "ensime/elisp/"))
 (require 'ensime)
-
+(require 'scala-mode2)
 ;; This step causes the ensime-mode to be started whenever
 ;; scala-mode is started for a buffer. You may have to customize this step
 ;; if you're not using the standard scala mode.
