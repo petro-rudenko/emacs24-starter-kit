8f2fd57483fbfdd164231e2b8c9605c0c5c4e2a2
Peter <petro.rudenko@gmail.com>
2013-04-11 02:12:30 +0300

    Update init.el to be compatible with emacs 24.3

--- init.el
+++ init.el
@@ -6,10 +6,13 @@
 ;;
 
 ;; remember this directory
-(setq starter-kit-dir
-      (file-name-directory (or load-file-name (buffer-file-name))))
-
-;; load up the starter kit
-(org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))
+(add-hook 'after-init-hook
+ `(lambda ()
+    ;; remember this directory
+    (setq starter-kit-dir
+          ,(file-name-directory (or load-file-name (buffer-file-name))))
+    ;; load up the starter kit
+    (require 'org)
+    (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))))
 
 ;;; init.el ends here
